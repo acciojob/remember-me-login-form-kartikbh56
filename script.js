842d83dbe962bd66fb98ec721408fd958b8eb7e6
Kartik B H <95575463+kartikbh56@users.noreply.github.com>
2025-07-06 23:04:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your JS code here. If required.
+window.addEventListener("DOMContentLoaded", () => {
+  const existing = document.getElementById("existing");
+  const username = localStorage.getItem("username");
+  const password = localStorage.getItem("password");
+
+  if (username && password) {
+    existing.style.display = "inline-block";
+  }
+});
+
 alert("Logged in as <username>");
